d6b4a259823f1cc467252a406905901efadbe64d
DESKTOP-0AG12GL\janma <janmarcnello@gmail.com>
2025-04-04 12:19:30 +0200

    global js millorat AVP

--- public/js/global.js
+++ public/js/global.js
@@ -15,30 +15,50 @@ const inicialitzarDades = () => {
               }
           ],
           jocs: [
-              {
-                  id: 1,
-                  nom: "Super Mario Bros",
-                  consola: "NES",
-                  any: 1985,
-                  descripcio: "Clàssic joc de plataformes de Nintendo.",
-                  url: "https://exemple.com/mario",
-                  categoria: "plataformes",
-                  monedes: "2000",
-                  dificultat: "facil",
-                  vegadesJugat: "20",
+            {
+                id: 1,
+                nom: "Penjat",
+                creadors: "Alex, Alejandro",
+                mini-descripcio:"El joc del penjat és un joc de paraules on un jugador ha d'endevinar una paraula lletra per lletra abans que es completi el dibuix d'un penjat." ,
+                descripcio: "El joc del penjat és un clàssic joc de paraules on un jugador intenta endevinar una paraula secreta abans d'esgotar un nombre limitat d'intents. Un jugador escull una paraula i l'altre ha d'endevinar-la suggerint lletres. Si una lletra forma part de la paraula, es revela la seva posició; si no, es dibuixa una part del penjat. El joc acaba quan s'endevina la paraula o quan el dibuix del penjat es completa. És un joc senzill però entretingut que ajuda a millorar el vocabulari i la memòria.",
+                url: "/Penjat/index.html",
+                categoria: "trencaclosques"
               },
               {
-                  id: 2,
-                  nom: "Street Fighter II",
-                  consola: "Arcade",
-                  any: 1991,
-                  descripcio: "Famosíssim joc de lluita.",
-                  url: "https://exemple.com/streetfighter",
-                  categoria: "lluita",
-                  monedes: "100",
-                  dificultat: "facil",
-                  vegadesJugat: "20",
-              }
+                id: "2",
+                nom: "NOM",
+                creadors: "Creadors",
+                mini-descripcio:"Descripcio de 1 linea o 2 aprox" ,
+                Descripcio: "Descripcio completa del joc y mecaniques",
+                url : "/nom del joc/index.html",
+                categoria: "la categoria del joc"
+              },
+              {
+                id: "3",
+                nom: "NOM",
+                creadors: "Creadors",
+                mini-descripcio:"Descripcio de 1 linea o 2 aprox" ,
+                Descripcio: "Descripcio completa del joc y mecaniques",
+                url : "/nom del joc/index.html",
+                categoria: "la categoria del joc"
+              },
+              {
+                id: "4",
+                nom: "NOM",
+                creadors: "Creadors",
+                mini-descripcio:"Descripcio de 1 linea o 2 aprox" ,
+                Descripcio: "Descripcio completa del joc y mecaniques",
+                url : "/nom del joc/index.html",
+                categoria: "la categoria del joc"
+              {
+                id: "5",
+                nom: "NOM",
+                creadors: "Creadors",
+                mini-descripcio:"Descripcio de 1 linea o 2 aprox" ,
+                Descripcio: "Descripcio completa del joc y mecaniques",
+                url : "/nom del joc/index.html",
+                categoria: "la categoria del joc"
+              },
           ],
           partides: [
             {
